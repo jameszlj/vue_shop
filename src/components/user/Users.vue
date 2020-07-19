5e8719aadb37bafa8bf26f05c1be8d5cf9cb58ec
james.zhan@3ti.us <james.zhan@3ti.us>
2020-07-19 19:56:30 +0800

    menu

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    <h3>用户列表组件</h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
 }
 </script>
 
-<style>
+<style lang="" scoped>
 </style>
